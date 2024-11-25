293c283b655a6135d26ba41a184d08f2f66c8743
Aggron282 <marcokhodr1@gmail.com>
2024-11-25 01:55:25 -0700

    S

--- assets/js/start_game.js
+++ assets/js/start_game.js
@@ -1,37 +1,41 @@
-var start_container = document.querySelector(".start_container");
-var start_btn = document.querySelector(".start_btn");
-var main_container = document.querySelector(".main_container");
+// var start_container = document.querySelector(".start_container");
+// var start_btn = document.querySelector(".start_btn");
+// var main_container = document.querySelector(".main_container");
 
-var watermark_container = document.querySelector(".watermark_container");
+// var watermark_container = document.querySelector(".watermark_container");
 
-var isPlaying = false;
+// var isPlaying = false;
 
-start_btn.addEventListener("click",(e)=>{
-  isPlaying = true;
-  StartGame();
-});
+// start_btn.addEventListener("click",(e)=>{
+//   isPlaying = true;
+//   StartGame();
+// });
 
-const ToggleScreens = (isPlaying_) =>{
-
-  if(isPlaying){
-    start_container.classList.add("inactive");
-    main_container.classList.remove("inactive");
-    watermark_container.classList.add("inactive--watermark")
-  }
-  else{
-    main_container.classList.add("inactive");
-    start_container.classList.remove("inactive");
-    watermark_container.classList.remove("inactive--watermark")
-  }
-
-}
+// const ToggleScreens = (isPlaying_) =>{
+//
+//   if(isPlaying){
+//     start_container.classList.add("inactive");
+//     main_container.classList.remove("inactive");
+//     watermark_container.classList.add("inactive--watermark")
+//   }
+//   else{
+//     main_container.classList.add("inactive");
+//     start_container.classList.remove("inactive");
+//     watermark_container.classList.remove("inactive--watermark")
+//   }
+//
+// }
 
 const StartGame = () => {
-  ToggleScreens();
+
   RenderOptions(true);
   RenderOptions(false);
   HighlightEnemyOptions();
   AddEventsToPlayerChoice();
+
 }
-isPlaying = false;
-ToggleScreens();
+
+// isPlaying = false;
+
+
+StartGame();
